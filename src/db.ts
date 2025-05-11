cd39591fedd8592e365ad537b013cfcf77e06338
test <test>
2025-05-11 23:47:54 +0900

    NoteInfoを追加

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import Database from "better-sqlite3";
-import { Note } from "./Note";
+import { Note } from "./type/Note";
+import { NoteInfo } from "./type/NoteInfo";
 
+// 登録されているNoteのUUIDを全て取得
 export const getIdList = (dbPath: string): Array<string> => {
     const db = new Database(dbPath);
 
@@ -9,6 +11,7 @@ export const getIdList = (dbPath: string): Array<string> => {
 
     for(const item of stmt.iterate()) {
         const note = item as Record<keyof Note, unknown> 
+
         if (typeof note.Id === "string") {
             idList.push(note.Id);
         }
@@ -17,4 +20,12 @@ export const getIdList = (dbPath: string): Array<string> => {
     db.close();
 
     return idList;
+}
+
+// 登録されているNoteの一覧を取得
+export const getNotesList = (dbPath: string):Array<NoteInfo> => {
+
+    const noteInfoList = new Array<NoteInfo>();
+
+    return noteInfoList;
 }
