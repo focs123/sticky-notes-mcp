495754d754d0744153b3ec9009d1150c94ebdb3e
test <test>
2025-05-11 20:11:21 +0900

    デモ作成

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+// MCPサーバーのインスタンスを作成
+const server = new McpServer({
+    name: 'uuid-generator', // サーバー名
+    version: '1.0.0', // バージョン
+});
+
+// MCPのレスポンス形式に従ってUUIDを返却
+server.tool('generate_uuid', 'UUIDを生成する。', async () => {
+    const uuid = crypto.randomUUID();
+
+    return {
+        content: [{
+            type: 'text',
+            text: `生成されたUUID: ${uuid}`,
+        }],
+    }
+});
+
+
+// メイン関数を定義 sqliteのパスとかを設定？
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+
+    console.log('UUID Generator MCP Server running on stdio');
+}
+
+main().catch(error => {
+    console.error('Fatal error in main():', error)
+    process.exit(1);
+});
